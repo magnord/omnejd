05a0098d9895e92156327ff71fec2cf34a19ee95
Peter Bengtson <peter@peterbengtson.com>
2009-04-19 12:52:58 +0200

    Perishable tokens (for things like password resets) are now set to live for 30 minutes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      flash[:notice] = "Account registered!"
+      flash[:notice] = "Account registered"
       redirect_back_or_default account_url
     else
       render :action => :new
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
   def update
     @user = @current_user # makes our views "cleaner" and more consistent
     if @user.update_attributes(params[:user])
-      flash[:notice] = "Account updated!"
+      flash[:notice] = "Account updated"
       redirect_to account_url
     else
       render :action => :edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   acts_as_authentic do |c|
     # for available options see documentation in: Authlogic::ActsAsAuthentic
     c.logged_in_timeout = 30.minutes
+    c.perishable_token_valid_for = 30.minutes
   end
   
   
