30de60ae981802c92f0c563ae8e57866fabd39ff
Magnus Nordin <magnord@gmail.com>
2009-04-19 14:39:50 +0200

    Unpacked gem GeoRuby

--- config/environment.rb
+++ config/environment.rb
@@ -22,8 +22,8 @@ Rails::Initializer.run do |config|
   config.gem "authlogic"
   config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
   config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
+  config.gem "GeoRuby", :lib => "geo_ruby"
   config.gem "nofxx-postgis_adapter", :lib => "postgis_adapter", :source => "http://gems.github.com"
-  #config.gem "GeoRuby"
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
