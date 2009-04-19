9fe7df2f58d097d6e15308878afc165c0aa77c7b
Peter Bengtson <peter@peterbengtson.com>
2009-04-19 12:13:38 +0200

    Signup, login, logout, etc, now works. No email confirmation yet.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   
   acts_as_authentic do |c|
-    #c.my_config_option = my_value # for available options see documentation in: Authlogic::ActsAsAuthentic
-  end # block optional
+    # for available options see documentation in: Authlogic::ActsAsAuthentic
+    c.logged_in_timeout = 30.minutes
+  end
   
 end
