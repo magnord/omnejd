7bb2b72b6eecf04b47a1f287fe2800296e247235
Magnus Nordin <magnord@gmail.com>
2009-04-20 15:01:32 +0200

    Added gem "redgreen"

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,3 +30,4 @@ config.action_mailer.delivery_method = :test
 config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
 config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
 config.gem "jeremymcanally-matchy", :lib => "matchy", :source => "http://gems.github.com"
+config.gem "redgreen"
