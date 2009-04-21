ed040aebf577410ab2d4d80f3034a44b69a70588
Magnus Nordin <magnord@gmail.com>
2009-04-21 17:39:40 +0200

    Strange. "git commit -a -v" did take. Doing it again with explicit git add.

---
+++ spec/blueprints.rb
@@ -0,0 +1,29 @@
+require 'faker'
+
+Sham.login { |n| "user#{n}" }
+Sham.name { |n| Faker::Lorem.words(n) }
+Sham.email { Faker::Internet.email }
+Sham.title { Faker::Lorem.sentence }
+Sham.body  { Faker::Lorem.paragraph }
+Sham.pos { |n| Point.from_x_y(n,n) }
+Sham.geom {|n| Polygon.from_coordinates([[[n*5,n*5],[n*5,n*5+5],[n*5+5,n*5+5],[n*5+5,n*5],[n*5,n*5]]])}
+
+User.blueprint do
+  login
+  email
+  password "password"
+  password_confirmation "password"
+end
+
+Post.blueprint do
+  title
+  body
+  user
+  pos 
+end
+
+Area.blueprint do
+  name  
+  geom 
+end
+
