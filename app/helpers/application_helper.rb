04750882eba14c2836078a1345c4e6283b351ed9
Peter Bengtson <peter@peterbengtson.com>
2009-04-30 22:13:57 +0200

    Added fancy button helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,20 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def submit_button(text='Submit')
+    content_tag :button, image_tag('/stylesheets/blueprint/plugins/buttons/icons/tick.png', :alt => '') + ' ' + text, 
+                :type => 'submit', :class => 'button positive'
+  end
+  
+  def cancel_button(text, path)
+    link_to image_tag("/stylesheets/blueprint/plugins/buttons/icons/cross.png", :alt => '') + ' ' + text, path,
+            :class => 'button negative'
+  end
+  
+  def change_password_button(text, path)
+    link_to image_tag("/stylesheets/blueprint/plugins/buttons/icons/key.png", :alt => '') + ' ' + text, path,
+            :class => 'button'
+  end
+
 end
+
