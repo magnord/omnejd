8da024e8869c67e05314d06bc169fd15cf961ab1
Peter Bengtson <peter@peterbengtson.com>
2009-05-01 12:24:06 +0200

    TinyMCE added. Old version - will update to the most recent one today.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,42 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
+  #
+  # Button to be used inside a form, to submit it
+  #
   def submit_button(text='Submit')
     content_tag :button, image_tag('/stylesheets/blueprint/plugins/buttons/icons/tick.png', :alt => '') + ' ' + text, 
                 :type => 'submit', :class => 'button positive'
   end
   
+  
+  #
+  # General button for cancel operations, etc
+  #
   def cancel_button(text, path)
     link_to image_tag("/stylesheets/blueprint/plugins/buttons/icons/cross.png", :alt => '') + ' ' + text, path,
             :class => 'button negative'
   end
   
+  
+  #
+  # Specialized button for password-related stuff
+  #
   def change_password_button(text, path)
     link_to image_tag("/stylesheets/blueprint/plugins/buttons/icons/key.png", :alt => '') + ' ' + text, path,
             :class => 'button'
+  end
+
+
+  #
+  # Use this at the top of views to activate TinyMCE editor functionality
+  #
+  def use_tinymce
+    # Avoid multiple inclusions
+    @content_for_tinymce = ""
+    content_for :tinymce do
+      javascript_include_tag('tiny_mce/tiny_mce') + javascript_include_tag('mce_editor')
+    end
   end
 
 end
