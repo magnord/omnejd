4bf744454a80dc82771f42c6c4dbd6d1c756bf0e
Peter Bengtson <peter@peterbengtson.com>
2009-05-16 11:13:50 +0200

    Changes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  
   include OmnejdMap
+  
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,4 +40,3 @@ module ApplicationHelper
   end
 
 end
-
